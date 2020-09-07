b38ca81703357e0533cada512d5b8a34efca67d1
PW <pwelliver@yahoo.com>
2020-09-07 20:09:48 +0200

    both functional and class components and contex provided

--- src/contexts/ThemeContext-func.js
+++ src/contexts/ThemeContext-func.js
@@ -3,18 +3,17 @@ import React, { createContext, useState } from 'react';
 export const ThemeContext = createContext();
 
 const ThemeContextProvider = (props) => {
-  const [isLightTheme, setIsLightTheme] = useState(true);
+  const [isLightTheme, setIsLightTheme] = useState({ isLightTheme: true });
   const [light, setLight] = useState({
     syntax: '#555',
     ui: '#ddd',
     bg: '#eee',
   });
-
   const [dark, setDark] = useState({ syntax: '#ddd', ui: '#333', bg: '#555' });
 
   const toggleTheme = () => {
     console.log('Toggle');
-    setIsLightTheme(!isLightTheme);
+    setIsLightTheme({ isLightTheme: !isLightTheme });
   };
 
   return (
